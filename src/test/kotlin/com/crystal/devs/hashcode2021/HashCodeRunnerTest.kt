1a8a51a5996a7173e704382d9fa93204a1145df6
Tom Pietri <tom.pietri-ext@pole-emploi.fr>
2022-02-24 18:48:31 +0100

    add inpuyt

--- src/test/kotlin/com/crystal/devs/hashcode2021/HashCodeRunnerTest.kt
+++ src/test/kotlin/com/crystal/devs/hashcode2021/HashCodeRunnerTest.kt
@@ -8,33 +8,33 @@ import java.io.File
 internal class HashCodeRunnerTest {
     @Test
     fun `run Input A`() {
-        val input = File("./src/main/resources/a_example.in").readLines(Charsets.UTF_8)
+        val input = File("./src/main/resources/a_an_example.in.txt").readLines(Charsets.UTF_8)
         assertThat(runInput(input, "A")).isGreaterThan(0)
     }
 
     @Test
     fun `run Input B`() {
-        val input = File("./src/main/resources/b_small.in").readLines(Charsets.UTF_8)
+        val input = File("./src/main/resources/b_better_start_small.in.txt").readLines(Charsets.UTF_8)
         assertThat(runInput(input, "B")).isGreaterThan(0)
     }
 
     @Test
     fun `run Input C`() {
-        val input = File("./src/main/resources/c_medium.in").readLines(Charsets.UTF_8)
+        val input = File("./src/main/resources/c_collaboration.in.txt").readLines(Charsets.UTF_8)
         assertThat(runInput(input, "C")).isGreaterThan(0)
     }
 
 
     @Test
     fun `run Input D`() {
-        val input = File("./src/main/resources/d_quite_big.in").readLines(Charsets.UTF_8)
+        val input = File("./src/main/resources/d_dense_schedule.in.txt").readLines(Charsets.UTF_8)
         assertThat(runInput(input, "D")).isGreaterThan(0)
     }
 
 
     @Test
     fun `run Input E`() {
-        val input = File("./src/main/resources/e_also_big.in").readLines(Charsets.UTF_8)
+        val input = File("./src/main/resources/e_exceptional_skills.in.txt").readLines(Charsets.UTF_8)
         assertThat(runInput(input, "E")).isGreaterThan(0)
     }
 }
