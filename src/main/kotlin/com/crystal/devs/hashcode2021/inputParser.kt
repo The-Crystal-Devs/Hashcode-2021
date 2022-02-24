f788e04455150d49ee6d718f137ee97cdad30ac7
Tom Pietri <tom.pietri-ext@pole-emploi.fr>
2022-02-24 19:02:45 +0100

    parse input

--- src/main/kotlin/com/crystal/devs/hashcode2021/inputParser.kt
+++ src/main/kotlin/com/crystal/devs/hashcode2021/inputParser.kt
@@ -1,7 +1,45 @@
 package com.crystal.devs.hashcode2021
 
 fun parseInput(input: List<String>): ParsedInput {
-    return ParsedInput(input = input)
+    val (nbContributors, nbProjects) = input.first().split(" ").map { it.toInt() }
+    val contributors = mutableListOf<Contributor>()
+    val drop = input.drop(1)
+    var i = 0
+    while (contributors.size < nbContributors) {
+        val (name, skillNumber) = drop[i].split(" ")
+        val skills =  (1..skillNumber.toInt()).map {
+            val (skillName, skillLevel) = drop[i + it].split(" ")
+            Skill(skillName, skillLevel.toInt())
+        }
+
+        contributors.add(Contributor(name, skills))
+        i += 1 + skillNumber.toInt()
+    }
+
+    val projectsInput = input.drop(1 + i)
+    val projects = mutableListOf<Project>()
+    i = 0
+    while (projects.size < nbProjects) {
+        val split = projectsInput[i].split(" ")
+        val projectName = split[0]
+        val (duration, score, bestBefore, nbSkils) = split.drop(1).map { it.toInt() }
+        val skills =  (1..nbSkils).map {
+            val (skillName, skillLevel) = projectsInput[i + it].split(" ")
+            Skill(skillName, skillLevel.toInt())
+        }
+
+        projects.add(Project(projectName, duration, score, bestBefore, skills))
+        i += 1 + nbSkils
+    }
+
+    return ParsedInput(contributors, projects)
 }
 
-data class ParsedInput(val input : List<String>);
+data class ParsedInput(
+    val contributors: List<Contributor>,
+    val projects: List<Project>
+    )
+
+data class Contributor(val name: String, val skills: List<Skill>)
+data class Skill(val skillName: String, val level: Int)
+data class Project(val projectName: String, val duration: Int, val score: Int, val bestBeforeTime: Int, val roles: List<Skill>)
