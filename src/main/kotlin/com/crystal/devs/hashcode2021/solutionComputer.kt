4a6dd5bae861723a77a732b716d19d9501cc564e
Tom Pietri <tom.pietri-ext@pole-emploi.fr>
2022-02-24 21:02:10 +0100

    wip

--- src/main/kotlin/com/crystal/devs/hashcode2021/solutionComputer.kt
+++ src/main/kotlin/com/crystal/devs/hashcode2021/solutionComputer.kt
@@ -4,12 +4,12 @@ fun computeSolution(parsedInput: ParsedInput): Solution {
     val maxPossibleDays = parsedInput.projects.maxBy { it.bestBeforeTime + it.score }
 
     val weightedProjects = parsedInput.projects.associateWith { it.computeWeight() }
-    val sortedProjects = weightedProjects.entries.sortedBy { it.value }.toMutableList()
+    val sortedProjects = weightedProjects.entries.sortedByDescending { it.value }.toMutableList()
     val availableContributors = parsedInput.contributors.toMutableSet()
     val finishedProjects = mutableListOf<FinishedProject>()
 
     while(sortedProjects.isNotEmpty()) {
-        val project = sortedProjects.removeAt(0).key
+        val project = sortedProjects.first().key
         val filledProject = associateContributorsToSkill(project.roles, availableContributors)
 
         if(filledProject.isNotEmpty()) {
@@ -23,8 +23,9 @@ fun computeSolution(parsedInput: ParsedInput): Solution {
                 val contributor = availableContributors.find { contributor -> contributor.name == it.second }
                 if(it.first.level == contributor!!.skills[it.first.name]) {
                     contributor.skills[it.first.name] = contributor.skills[it.first.name]!! + 1
-                    contributor.skills.compute(it.first.name) { k, v -> (v ?: 0) + 1 }
+                    contributor.skills.compute(it.first.name) { _, v -> (v ?: 0) + 1 }
                 }
+                sortedProjects.remove(project)
             }
 
             availableContributors.removeIf { filledProject.map { p -> p.second }.contains(it.name) }
@@ -48,6 +49,12 @@ fun computeSolution(parsedInput: ParsedInput): Solution {
     return Solution(finishedProjects)
 }
 
+fun toto(currentDay) {
+
+}
+
+data class ProcessDay
+
 fun associateContributorsToSkill(roles: List<Skill>, availableContributors: Set<Contributor>): List<Pair<Skill, String>> {
     val skillToPossibleContributor = roles.associateWith { role ->
         val possibleContributors = availableContributors.filter { (it.skills[role.name] ?: 0) >= role.level }
@@ -61,8 +68,8 @@ fun associateContributorsToSkill(roles: List<Skill>, availableContributors: Set<
     val skillWithContributorsSortedByNbContributors = skillToPossibleContributor.entries.sortedByDescending { it.value.size }
     val chosenSkill = skillWithContributorsSortedByNbContributors.first()
     val remainingRoles = roles.filter { it != chosenSkill.key }
-    for (i in 0 until chosenSkill.value.size) {
-        val chosenContributor = chosenSkill.value[i]
+    val sortedContributors = chosenSkill.value.sortedBy { it.skills[chosenSkill.key.name] }
+    for (chosenContributor in sortedContributors) {
         if(roles.size == 1) {
             return listOf((chosenSkill.key to chosenContributor.name))
         }
@@ -86,7 +93,12 @@ data class Contributor(val name: String, val skills: MutableMap<String , Int>)
 data class Skill(val name: String, val level: Int)
 data class Project(val projectName: String, val duration: Int, val score: Int, val bestBeforeTime: Int, val roles: List<Skill>) {
     fun computeWeight() : Double {
-        return score.toDouble() / duration.toDouble()
+//        return bestBeforeTime.toDouble()
+        return score.toDouble() / duration.toDouble() * (1.0 / computeDifficulty())
+    }
+
+    fun computeDifficulty() : Double {
+        return roles.sumBy { it.level }.toDouble()
     }
 };
 
